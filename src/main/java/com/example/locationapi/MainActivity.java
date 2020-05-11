20e66b8b322fcc3863c23b8e17f6fe37e5566cb3
vladimirKartyshov <vovan198080@inbox.ru>
2020-05-11 08:57:47 +0300

    buildLocationRequest();   buildLocationCallback();    buildlocationSettingsRequest();

--- src/main/java/com/example/locationapi/MainActivity.java
+++ src/main/java/com/example/locationapi/MainActivity.java
@@ -1,15 +1,38 @@
 package com.example.locationapi;
 
+import android.location.Location;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.SettingsClient;
+
+import java.text.DateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button startLocationUpdatesButton, stoptLocationUpdatesButton;
     private TextView locationTextView;
+    private TextView locationUpdateTimeTextView;
+
+    private FusedLocationProviderClient fusedLocationClient;
+    private SettingsClient settingsClient;
+    private LocationRequest locationRequest;
+    private LocationSettingsRequest locationSettingsRequest;
+    private LocationCallback locationCallback;
+    private Location currentLocation;
+
+    private boolean isLocationUpdatesActive;
+    private String locationUpdateTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +42,47 @@ public class MainActivity extends AppCompatActivity {
         startLocationUpdatesButton = findViewById(R.id.startLocationUpdatesButton);
         stoptLocationUpdatesButton = findViewById(R.id.stoptLocationUpdatesButton);
         locationTextView = findViewById(R.id.locationTextView);
+        locationUpdateTimeTextView = findViewById(R.id.locationUpdateTimeTextView);
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        settingsClient = LocationServices.getSettingsClient(this);
+
+        buildLocationRequest();
+        buildLocationCallback();
+        buildlocationSettingsRequest();
+    }
+
+    private void buildlocationSettingsRequest() {
+        LocationSettingsRequest.Builder builder =
+                new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(locationRequest);
+        locationSettingsRequest = builder.build();
+
+    }
+
+    private void buildLocationCallback() {
+        locationCallback = new LocationCallback(){
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+
+                currentLocation = locationResult.getLastLocation();
+
+                locationTextView.setText("" +
+                        currentLocation.getLatitude() + "/" +
+                        currentLocation.getLongitude()
+                );
+                locationUpdateTimeTextView.setText(
+                        DateFormat.getTimeInstance().format(new Date())
+                );
+            }
+        };
+    }
+
+    private void buildLocationRequest() {
+        locationRequest = new LocationRequest();
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(3000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
     }
 }
